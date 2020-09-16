b92523fde1f0f6da9e93c8fa5e17d6e5200fe2a1
Firefly718 <System.BLA.16@yandex.ru>
2020-09-16 17:12:01 +0300

    finished nav menu

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ $(document).ready(function(){
       $("body > header > div > div > div.header__contact > div.header__contact--second > div > ul").toggle('slow', 'swing');
     });
 });
+
+// search icon in nav menu
+// $(document).ready(function(){
+//   $("body > header > nav > div > div > a:last-child").click(function(){
+//     $("body > header > nav > div > div > a:last-child > input[type=search]").toggle('slow', 'swing');
+//   });
+// });
+
+// document.querySelector("body > header > nav > div > div > a:nth-child(8)")
+// document.querySelector("body > header > nav > div > div > a:nth-child(8) > input[type=search]")
