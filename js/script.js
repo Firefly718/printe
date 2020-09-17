312c8d86f1a520ed3c35453ecbc1ee3e3df92c73
Firefly718 <System.BLA.16@yandex.ru>
2020-09-17 11:24:24 +0300

    fixed some mistakes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 // Phone list in header
 $(document).ready(function(){
-    $("body > header > div > div > div.header__contact > div.header__contact--second > div").click(function(){
-      $("body > header > div > div > div.header__contact > div.header__contact--second > div > ul").toggle('slow', 'swing');
+    $("body > header > div > div > div.header__contact > div.contact > div").click(function(){
+      $("body > header > div > div > div.header__contact > div.contact > div > ul").toggle('slow', 'swing');
     });
 });
 
+
 // Phone list in footer
 $(document).ready(function(){
-  $("body > footer > div > div > div:nth-child(4) > div > div").click(function(){
-    $("body > footer > div > div > div:nth-child(4) > div > div > ul").toggle('slow', 'swing');
+  $("body > footer > div.container > div > div:nth-child(4) > div > div").click(function(){
+    $("body > footer > div.container > div > div:nth-child(4) > div > div > ul").toggle('slow', 'swing');
   });
 });
